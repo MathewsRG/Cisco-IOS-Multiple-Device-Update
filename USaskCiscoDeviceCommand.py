25245d51aeaf07fdd4c8fc882a368ddc114dbec0
Ryan Mathews <r.mathews@hotmail.com>
2023-05-23 15:55:21 -0600

    Adjusting to netmiko

--- USaskCiscoDeviceCommand.py
+++ USaskCiscoDeviceCommand.py
@@ -1,17 +1,6 @@
 # Ryan Mathews
 # USask Network Programming Analyst
 # This script will update a list of Cisco IOS appliances to configure the same batch of commands on each
-# Version 1.0 - Initial Creation
+# Version 2.0 - Figuring out Netmiko as paramiko fucking sucks
 
-import paramiko
-
-# List of FQDNs to connect
-devices = [
-    "fqdn1.example.com",
-    "fqdn2.example.com",
-    # Add more FQDNs to the list
-]
-
-# Prompt for credentials
-username = input("Enter your username: ")
-password = input("Enter your password: ")
+from netmiko import ConnectHandler
